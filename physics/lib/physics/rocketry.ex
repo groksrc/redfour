65b41cb694d32217f33d3f781bbff0c86a5c65e4
Drew Cain <groksrc@gmail.com>
2016-03-27 13:56:14 -0500

    cleanup

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -19,13 +19,3 @@ defmodule Physics.Rocketry do
     end
 
 end
-
-defmodule Calcs do
-    def rounded_to_nearest_tenth(val) do
-      Float.ceil(val, 1)
-    end
-    
-    def convert_to_km(velocity) do
-        velocity / 1000
-    end 
-end
