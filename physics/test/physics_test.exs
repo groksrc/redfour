a1ffd245615f80dd2d8d99683f2b4a4bea9d4496
Drew Cain <groksrc@gmail.com>
2016-03-27 14:16:56 -0500

    ch6 refactoring

--- physics/test/physics_test.exs
+++ physics/test/physics_test.exs
@@ -3,11 +3,11 @@ defmodule PhysicsTest do
   doctest Physics
 
   test "calcs rounded_to_nearest_tenth" do
-    assert 3.2 == Calcs.rounded_to_nearest_tenth(3.1415)
+    assert 3.2 == Calcs.to_nearest_tenth(3.1415)
   end
   
   test "calcs convert_to_km" do
-    assert 1 == Calcs.convert_to_km(1000)
+    assert 1 == Calcs.to_km(1000)
   end
   
   test "escape_velocity of earth" do
@@ -17,4 +17,13 @@ defmodule PhysicsTest do
   test "escape_velocity of jupiter" do
     assert 358.3 == Physics.Rocketry.escape_velocity(%{mass: 4.18e27, radius: 4.34407e6})
   end 
+  
+  test "escape_velocity of mars" do
+    assert 5.1 == Physics.Rocketry.escape_velocity(:mars)
+  end
+  
+  test "escape_velocity of the moon" do
+    assert 2.4 == Physics.Rocketry.escape_velocity(:moon)
+  end
+  
 end
