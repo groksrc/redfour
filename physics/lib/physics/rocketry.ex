3eb04daeb8d00b03588f880eaecc17be25e4b7a7
Drew Cain <groksrc@gmail.com>
2016-03-27 13:55:41 -0500

    moved calcs to its own file

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -29,25 +29,3 @@ defmodule Calcs do
         velocity / 1000
     end 
 end
-
-# defmodule Physics.Planet do
-    
-#     defstruct [
-#         name: "Earth",
-#         radius: 6.317e6,
-#         mass: 5.97e24        
-#     ]
-    
-#     def escape_velocity(planet) do
-#         g = 6.67e-11
-#         gmr = 2 * g * planet.mass_kg/planet.radius_m
-#         vms = :math.sqrt gmr 
-#         vkms = vms/1000
-#         Float.ceil vkms, 1
-#     end
-# end
-
-# v = %Physics.Planet{}
-#  |> Physics.Planet.escape_velocity
- 
-# v == 11.2
