2fb9d0ad63c0c8afb0be220a1968902a4d502edd
Drew Cain <drew@simpli.fi>
2016-03-28 19:48:56 -0500

    working with lists

--- physics/lib/solar.ex
+++ physics/lib/solar.ex
@@ -5,4 +5,14 @@ defmodule Solar do
     def power(classification, scale) do
         classification * scale 
     end        
+    
+    def total_flare_power(flares) do
+      Enum.map(flares, &(power(&1)))
+        |> Enum.sum
+    end 
+    
+    def flare_power_sum(flares), do: flare_power_sum(flares, 0)
+    def flare_power_sum([], total), do: total
+    def flare_power_sum([head | tail], total), do: flare_power_sum(tail, power(head) + total)
+      
 end
--- physics/test/solar_test.exs
+++ physics/test/solar_test.exs
@@ -4,14 +4,14 @@ defmodule SolarTest do
 
   setup do
     flares = [
-      %{classification: :X, scale: 99, date: Date.from({1859, 8, 29})},
-      %{classification: :M, scale: 5.8, date: Date.from({2015, 1, 12})},
-      %{classification: :M, scale: 1.2, date: Date.from({2015, 2, 9})},
-      %{classification: :C, scale: 3.2, date: Date.from({2015, 4, 18})},
+      %{classification: :X, scale: 99,   date: Date.from({1859, 8, 29})},
+      %{classification: :M, scale: 5.8,  date: Date.from({2015, 1, 12})},
+      %{classification: :M, scale: 1.2,  date: Date.from({2015, 2, 9})},
+      %{classification: :C, scale: 3.2,  date: Date.from({2015, 4, 18})},
       %{classification: :M, scale: 83.6, date: Date.from({2015, 6, 23})},
-      %{classification: :C, scale: 2.5, date: Date.from({2015, 7, 4})},
-      %{classification: :X, scale: 72, date: Date.from({2012, 7, 23})},
-      %{classification: :X, scale: 45, date: Date.from({2003, 11, 4})},
+      %{classification: :C, scale: 2.5,  date: Date.from({2015, 7, 4})},
+      %{classification: :X, scale: 72,   date: Date.from({2012, 7, 23})},
+      %{classification: :X, scale: 45,   date: Date.from({2003, 11, 4})},
     ]
     {:ok, data: flares}
   end
@@ -31,5 +31,13 @@ defmodule SolarTest do
   test "power :C", %{data: flares} do 
     assert 99 == Solar.power(%{classification: :C, scale: 99})
   end
+
+  test "total_flare_power", %{data: flares} do
+    assert 216911.7 = Solar.total_flare_power(flares)
+  end 
+
+  test "flare_power_sum", %{data: flares} do
+    assert 216911.7 = Solar.flare_power_sum(flares)
+  end 
 
 end
